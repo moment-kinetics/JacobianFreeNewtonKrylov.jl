1f2ac407371b3edc29bc271db5880ae824cc53d2
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2026-03-11 11:12:36 +0000

    Upgrade nonlinear test to use similar "physical" system to the linear test. Avoid isapprox where the norm appears to give a larger error than `maximum(abs.())`. Note that tolerances must be relaxed roughly quadratically with increasing n, suggesting potential numerical issues with the test or implementation.

--- test/JacobianFreeNewtonKrylovTests.jl
+++ test/JacobianFreeNewtonKrylovTests.jl
@@ -10,8 +10,8 @@ using JacobianFreeNewtonKrylov
 end
 
 function linear_test(; n=16, max_nkrylov = 12, atol=1.0e-13, atol_expected=1.0e-5, nonlinear_max_iterations=100)
-    println("    - linear test")
-    @testset "linear test " begin
+    println("    - linear test n=$n")
+    @testset "linear test n=$n" begin
         # Test represents constant-coefficient diffusion, in 1D steady state, with a
         # central finite-difference discretisation of the second derivative.
         #
@@ -19,9 +19,7 @@ function linear_test(; n=16, max_nkrylov = 12, atol=1.0e-13, atol_expected=1.0e-
         # because the inexact Jacobian-vector product we use in linear_solve!() means
         # linear_solve!() on its own does not converge to the correct answer.
 
-        #n = 16
         restart = max_nkrylov
-        #atol = 1.0e-13
 
         # the grid z
         z = collect(0:n-1) ./ (n-1)
@@ -70,7 +68,7 @@ function linear_test(; n=16, max_nkrylov = 12, atol=1.0e-13, atol_expected=1.0e-
             nonlinear_max_iterations = nonlinear_max_iterations)
 
         newton_solve!(x, rhs_func!, nl_solver_params,
-            diagnose = true)
+            diagnose = false)
 
         x_direct = A \ b
 
@@ -84,11 +82,11 @@ function linear_test(; n=16, max_nkrylov = 12, atol=1.0e-13, atol_expected=1.0e-
     end
 end
 
-function nonlinear_test(;
+function nonlinear_test(; n = 16 , atol = 1.0e-14, max_nkrylov = 12,
             preconditioner_option::PreconditionerOptionType=no_preconditioner,
             nonlinear_max_iterations=100)
-    println("    - non-linear test: $preconditioner_option")
-    @testset "non-linear test: $preconditioner_option" begin
+    println("    - non-linear test: $preconditioner_option n=$n")
+    @testset "non-linear test: $preconditioner_option n=$n" begin
         # Test represents constant-coefficient diffusion, in 1D steady state, with a
         # central finite-difference discretisation of the second derivative.
         #
@@ -96,56 +94,68 @@ function nonlinear_test(;
         # because the inexact Jacobian-vector product we use in linear_solve!() means
         # linear_solve!() on its own does not converge to the correct answer.
 
-        n = 64
-        restart = 12
-        atol = 1.0e-15
+        restart = max_nkrylov
 
         # the grid z
         z = collect(0:n-1) ./ (n-1)
         # grid spacing
         Dz = z[2] - z[1]
-        # Delta t
-        Dt = 100.0
-        # RHS > 0
-        b = @. Dt * z * (1.0 - z)
+        # source s = - b > 0
+        b = @. - z * (1.0 - z)
+        # diffusion coefficient
+        Ddiffuse = 0.05
+        # including Dz spacing
+        Dk = (Ddiffuse / Dz^2)
 
+        # boundary conditions
+        b[1] = 1.0
+        b[n] = 1.0
+
+        # the solution vector
+        x = Array{Float64,1}(undef,n)
+        # initial condition (respects b.c. x[1]=x[n]=1.0)
+        x .= 1.0
+
+        # residual function
         function rhs_func!(residual, x)
+            # boundary condition row
             i = 1
-            D = 1.0 + abs(x[i])^2.5
-            residual[i] = x[i] - (Dt / Dz^2) * D * (- 2.0 * x[i] + x[i+1]) - b[i]
+            residual[i] = x[i] - b[i]
+            # central differences d^2/d z^2 for interior points
             for i ∈ 2:n-1
-                D = 1.0 + abs(x[i])^2.5
-                residual[i] = x[i] - (Dt / Dz^2) * D * (x[i-1] - 2.0 * x[i] + x[i+1]) - b[i]
+                Dx = abs(x[i])^2.5
+                residual[i] = Dk * Dx * (x[i-1] - 2.0 * x[i] + x[i+1]) - b[i]
             end
+            # boundary condition row
             i = n
-            D = 1.0 + abs(x[i])^2.5
-            residual[i] = x[i] - (Dt / Dz^2) * D * (x[i-1] - 2.0 * x[i]) / Dz^2 - b[i]
+            residual[i] = x[i] - b[i]
             return nothing
         end
 
-        A = zeros(n,n)
-        i = 1
-        A[i,i] = -2.0
-        A[i,i+1] = 1.0
-        for i ∈ 2:n-1
-            A[i,i-1] = 1.0
-            A[i,i] = -2.0
-            A[i,i+1] = 1.0
-        end
-        i = n
-        A[i,i-1] = 1.0
-        A[i,i] = -2.0
         # a preconditioner
-        P = zeros(n,n)
-        for i in eachindex(z)
-            P[i,i] = 1.0
-            @views @. P[i,:] -= (Dt / Dz^2) * A[i,:]
+        A = zeros(n,n)
+        function calculate_preconditioner!()
+            # boundary condition
+            i = 1
+            A[i,i] = 1.0
+            for i ∈ 2:n-1
+                Dx = abs(x[i])^2.5
+                A[i,i-1] = 1.0 * Dk * Dx
+                A[i,i] = -2.0 * Dk * Dx
+                A[i,i+1] = 1.0 * Dk * Dx
+            end
+            # boundary condition
+            i = n
+            A[i,i] = 1.0
+            return nothing
         end
+        # calculate the initial preconditioner
+        calculate_preconditioner!()
         dummyx = Array{Float64,1}(undef,n)
         # function to apply the preconditioner in place
         function preconditioner!(x)
             dummyx .= x
-            x .= P \ dummyx
+            x .= A \ dummyx
             return nothing
         end
 
@@ -161,26 +171,24 @@ function nonlinear_test(;
             right_preconditioner = identity
         end
 
-        # the solution vector
-        x = Array{Float64,1}(undef,n)
-        # initial condition
-        x .= 0.0
-
         nl_solver_params = nl_solver_info(
             length(x),
             rtol = 0.0,
             atol = atol,
             linear_restart = restart,
+            linear_rtol=0.0,
             nonlinear_max_iterations = nonlinear_max_iterations)
 
         newton_solve!(x, rhs_func!, nl_solver_params;
             left_preconditioner=left_preconditioner,
             right_preconditioner=right_preconditioner,
-            diagnose = true)
+            recalculate_preconditioner=calculate_preconditioner!,
+            diagnose = false)
 
         rhs_func!(nl_solver_params.residual, x)
 
-        @test isapprox(nl_solver_params.residual, zeros(n); atol=4.0*atol)
+        #@test isapprox(nl_solver_params.residual, zeros(n); atol=6.0*atol)
+        @test maximum(abs.(nl_solver_params.residual)) < 6.0*atol
     end
 end
 
@@ -191,9 +199,21 @@ function runtests()
         linear_test(n=32, atol_expected=(1.0/2.0)*1.0e-2)
         linear_test(n=64, atol_expected=(1.0/4.0)*1.0e-2, max_nkrylov=16)
         linear_test(n=128, atol_expected=(1.0/8.0)*1.0e-2, atol=1.0e-12, max_nkrylov=32)
-        #nonlinear_test(preconditioner_option=no_preconditioner, nonlinear_max_iterations=100)
-        #nonlinear_test(preconditioner_option=use_left_preconditioner, nonlinear_max_iterations=100)
-        #nonlinear_test(preconditioner_option=use_right_preconditioner, nonlinear_max_iterations=100)
+
+        nonlinear_test(preconditioner_option=no_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=32, atol=2.0e-14, preconditioner_option=no_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=64, atol=8.0e-14, preconditioner_option=no_preconditioner, nonlinear_max_iterations=250)
+        nonlinear_test(n=128, atol=32.0e-14, preconditioner_option=no_preconditioner, nonlinear_max_iterations=850)
+
+        nonlinear_test(preconditioner_option=use_left_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=32, atol=2.0e-14, preconditioner_option=use_left_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=64, atol=8.0e-14, preconditioner_option=use_left_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=128, atol=32.0e-14, preconditioner_option=use_left_preconditioner, nonlinear_max_iterations=100)
+
+        nonlinear_test(preconditioner_option=use_right_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=32, atol=2.0e-14, preconditioner_option=use_right_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=64, atol=8.0e-14, preconditioner_option=use_right_preconditioner, nonlinear_max_iterations=100)
+        nonlinear_test(n=128, atol=32.0e-14, preconditioner_option=use_right_preconditioner, nonlinear_max_iterations=100)
     end
 end
 
